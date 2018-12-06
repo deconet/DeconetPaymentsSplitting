d7d03695378477dc8b2b836e02dbf44febac0e47
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2018-12-06 14:07:26 -0800

    flattened

--- flatContracts/contracts/DeconetPaymentsSplittingFactory.sol.sol
+++ flatContracts/contracts/DeconetPaymentsSplittingFactory.sol.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity 0.4.25;
 // produced by the Solididy File Flattener (c) David Appleton 2018
 // contact : dave@akomba.com
 // released under Apache 2.0 licence
@@ -183,21 +183,23 @@ contract DeconetPaymentsSplitting {
     )
         external
     {
-        require(distributions.length == 0); // Make sure the clone isn't initialized yet.
-        require(_destinations.length <= 8 && _destinations.length > 0);  // max of 8 destinations
+        require(distributions.length == 0, "Contract can only be initialized once"); // Make sure the clone isn't initialized yet.
+        require(_destinations.length <= 8 && _destinations.length > 0, "There is a maximum of 8 destinations allowed");  // max of 8 destinations
         // prevent integer overflow when math with _sharesExponent happens
         // also ensures that low balances can be distributed because balance must always be >= 10**(sharesExponent + 2)
-        require(_sharesExponent <= 4);
+        require(_sharesExponent <= 4, "The maximum allowed sharesExponent is 4");
         // ensure that lengths of arrays match so array out of bounds can't happen
-        require(_destinations.length == _sharesMantissa.length);
+        require(_destinations.length == _sharesMantissa.length, "Length of destinations does not match length of sharesMantissa");
 
         uint sum = 0;
         for (uint i = 0; i < _destinations.length; i++) {
-            require(!isContract(_destinations[i])); // Forbid contract as destination so that transfer can never fail
+            // Forbid contract as destination so that transfer can never fail
+            require(!isContract(_destinations[i]), "A contract may not be a destination address");
             sum = sum.add(_sharesMantissa[i]);
             distributions.push(Distribution(_destinations[i], _sharesMantissa[i]));
         }
-        require(sum == 10**(_sharesExponent + 2)); // taking into account 100% by adding 2 to the exponent.
+         // taking into account 100% by adding 2 to the exponent.
+        require(sum == 10**(_sharesExponent.add(2)), "The sum of all sharesMantissa should equal 10 ** ( _sharesExponent + 2 ) but it does not.");
         sharesExponent = _sharesExponent;
         emit DistributionCreated(_destinations, _sharesMantissa, _sharesExponent);
     }
@@ -207,13 +209,21 @@ contract DeconetPaymentsSplitting {
      */
     function distributeFunds() public {
         uint balance = address(this).balance;
-        require(balance >= 10**(sharesExponent + 2));
+        require(balance >= 10**(sharesExponent.add(2)), "You can not split up less wei than sum of all shares");
         for (uint i = 0; i < distributions.length; i++) {
             Distribution memory distribution = distributions[i];
             uint amount = calculatePayout(balance, distribution.mantissa, sharesExponent);
             distribution.destination.transfer(amount);
             emit FundsOperation(distribution.destination, amount, FundsOperationType.Outgoing);
         }
+    }
+
+    /**
+     * @dev Returns length of distributions array
+     * @return Length of distributions array
+    */
+    function distributionsLength() public view returns (uint256) {
+        return distributions.length;
     }
 
 
@@ -225,7 +235,7 @@ contract DeconetPaymentsSplitting {
      * @return An uint of the payout.
      */
     function calculatePayout(uint _fullAmount, uint _shareMantissa, uint _shareExponent) public pure returns(uint) {
-        return (_fullAmount.div(10 ** (_shareExponent + 2))).mul(_shareMantissa);
+        return (_fullAmount.div(10 ** (_shareExponent.add(2)))).mul(_shareMantissa);
     }
 
     /**
